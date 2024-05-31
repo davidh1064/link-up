4e00491498585b3897c6c6c269aac5e76d7cd87d
davidh1064 <huangdavid10@gmail.com>
2024-05-30 17:37:00 -0700

    sign-up page setup with image and simple button

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,10 +1,28 @@
-/* grabs the signin and signup forms and our layout for them*/
-import React from 'react'
+/* grabs the signin and signup forms and our layout for them */
+import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
-  return (
-    <div>AuthLayout</div>
-  )
-}
+  const isAuthenticated = false;
 
-export default AuthLayout
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
+      )}
+    </>
+  );
+};
+
+export default AuthLayout;
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Button } from "@/components/ui/button";
 
 const SignupForm = () => {
   return (
-    <div>SignupForm</div>
-  )
-}
+    <div>
+      <Button>Sign Up</Button>
+    </div>
+  );
+};
 
-export default SignupForm
+export default SignupForm;
