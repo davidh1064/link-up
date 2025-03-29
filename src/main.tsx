b009a6ac52c4e001dd8ec6f24b9b443d00fa5897
davidh1064 <huangdavid10@gmail.com>
2025-03-28 23:27:24 -0700

    fix typo

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import ReachDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import AuthProvider from "./context/AuthContext";
 import { QueryProvider } from "./lib/react-query/QueryProvider";
 
-ReachDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <QueryProvider>
       <AuthProvider>
